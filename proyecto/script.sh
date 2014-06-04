a58b72c86d7a43b6290ee0311c7fc4e339b3a307
Fran <fran@fran-TOSHIBA-NB200.(none)>
2014-06-04 14:45:44 +0200

    Creacion usuarios

--- proyecto/script.sh
+++ proyecto/script.sh
@@ -2,28 +2,28 @@
 
 USERS="$(cat ./usuarios.txt)"
 
+#echo $USERS
+
 mkdir imagenes 2>/dev/null
 mkdir musica 2>/dev/null
 
-imagenes=( ./Muestra/Imagenes/*.jpg )
-N=${#imagenes[@]}
 
-canciones=( ./Muestra/Musica/*.mp3 )
-M=${#canciones[@]}
-
-for i in $USERS
+for user in $USERS
 do
-	mkdir ./imagenes/$i 2>/dev/null
-	mkdir ./musica/$i 2>/dev/null
-
-	((num=RANDOM%N))
-	randomfile=${imagenes[$num]}
-	cp "$randomfile" ./imagenes/$i
-
-	((num=RANDOM%M))
-	randomfile=${canciones[$num]}
-	cp "$randomfile" ./musica/$i
+	useradd $user
 done
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
