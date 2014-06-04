b3cfeb5953cdf3cd78b8ce97d4d2c2943695d41c
Fran <fran@fran-TOSHIBA-NB200.(none)>
2014-06-04 13:49:08 +0200

    Eleccion aleatoria de imagenes

--- proyecto/script.sh
+++ proyecto/script.sh
@@ -2,23 +2,19 @@
 
 USERS="$(cat ./usuarios.txt)"
 
-#echo $USERS
+mkdir imagenes 2>/dev/null
+mkdir musica 2>/dev/null
 
-mkdir imagenes
-mkdir musica
+imagenes=( ./Muestra/Imagenes/*.jpg )
+N=${#imagenes[@]}
 
-
-
-
-
-
-
-
-
-
-
-
-
+for i in $USERS
+do
+	mkdir ./imagenes/$i 2>/dev/null
+	((num=RANDOM%N))
+	randomfile=${imagenes[$num]}
+	cp $randomfile ./imagenes/$i
+done
 
 
 
