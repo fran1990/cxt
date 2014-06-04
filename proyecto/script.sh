601b772446f638ce332510d20aa4224e0b6f3b72
Fran <fran@fran-TOSHIBA-NB200.(none)>
2014-06-04 14:33:26 +0200

    Eleccion aleatoria de canciones

--- proyecto/script.sh
+++ proyecto/script.sh
@@ -8,12 +8,21 @@ mkdir musica 2>/dev/null
 imagenes=( ./Muestra/Imagenes/*.jpg )
 N=${#imagenes[@]}
 
+canciones=( ./Muestra/Musica/*.mp3 )
+M=${#canciones[@]}
+
 for i in $USERS
 do
 	mkdir ./imagenes/$i 2>/dev/null
+	mkdir ./musica/$i 2>/dev/null
+
 	((num=RANDOM%N))
 	randomfile=${imagenes[$num]}
-	cp $randomfile ./imagenes/$i
+	cp "$randomfile" ./imagenes/$i
+
+	((num=RANDOM%M))
+	randomfile=${canciones[$num]}
+	cp "$randomfile" ./musica/$i
 done
 
 
