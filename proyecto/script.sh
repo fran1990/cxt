a1739b963755fc14e0fecc9ab7c219104faf0dbb
Fran <fran@fran-TOSHIBA-NB200.(none)>
2014-06-04 14:48:56 +0200

    Creacion usuarios

--- proyecto/script.sh
+++ proyecto/script.sh
@@ -4,9 +4,14 @@ USERS="$(cat ./usuarios.txt)"
 
 #echo $USERS
 
-mkdir imagenes
-mkdir musica
+mkdir imagenes 2>/dev/null
+mkdir musica 2>/dev/null
 
+
+for user in $USERS
+do
+	useradd $user
+done
 
 
 
